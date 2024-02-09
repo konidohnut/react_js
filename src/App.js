0ee8695a6b4b303c1cc7e85d92180b1c20c92ac7
backendhousewife <valeria.novogran.work@gmail.com>
2024-02-09 16:23:14 +0300

    Build: lesson 7

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+      <div>
+    <Header/>
+    <Technologies/>
+      </div>);
+}
+
+const Technologies = () =>{
+          return (
+              <div>
+                  <ul>
+                      <li>css</li>
+                      <li>html</li>
+                      <li>js</li>
+                      <li>react</li>
+                  </ul>
+              </div>
+          );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href='#s'>Home</a>
+            <a href='#s'>News Feed</a>
+            <a href='#s'>Messages</a>
+        </div>
+    );
+}
+
+export default App;
